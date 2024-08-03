bf243c7609817371a99a12256f500d57884e1115
unknown <miranam2004@gmail.com>
2024-08-03 20:31:16 +0530

    frontend completed

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Home from "./Home/Home";
+import { Route, Routes } from "react-router-dom";
+import Courses from "./courses/Courses";
+import Signup from "./components/Signup";
+
+const App = () => {
+  return (
+    <>
+      <div className="dark:bg-slate-900 dark:text-white">
+        <Routes>
+          <Route path="/" element={<Home></Home>}></Route>
+          <Route path="/course" element={<Courses></Courses>}></Route>
+          <Route path="/signup" element={<Signup></Signup>}></Route>
+        </Routes>
+      </div>
+    </>
+  );
+};
+
+export default App;
